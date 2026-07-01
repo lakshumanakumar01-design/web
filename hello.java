d9656185e08e5da248a5c76dac3f5a55ebfe1097
lk <lakshumanakumar01@gmail.com>
2026-07-02 01:49:36 +0530

    bug2

--- hello.java
+++ hello.java
@@ -4,6 +4,7 @@ public class hello {
         int a = 5;
         int b = 10;
         int sum = a + b;
+          
         System.out.println("The sum of " + a + " and " + b + " is: " + sum);
     }
     
